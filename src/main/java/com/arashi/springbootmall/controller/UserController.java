238ffd377db3a3d386fe1ea6fc68351f569ee581
ArashiPurpleMoon <thmko480@gmail.com>
2025-03-24 19:01:29 +0800

    帳號功能 - 登入

--- src/main/java/com/arashi/springbootmall/controller/UserController.java
+++ src/main/java/com/arashi/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.arashi.springbootmall.controller;
 
+import com.arashi.springbootmall.dto.UserLoginRequest;
 import com.arashi.springbootmall.dto.UserRegisterRequest;
 import com.arashi.springbootmall.model.User;
 import com.arashi.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/arashi/springbootmall/service/UserService.java
+++ src/main/java/com/arashi/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.arashi.springbootmall.service;
 
+import com.arashi.springbootmall.dto.UserLoginRequest;
 import com.arashi.springbootmall.dto.UserRegisterRequest;
 import com.arashi.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     Integer register(UserRegisterRequest userRegisterRequest);
 
     User getUserById(Integer userId);
+
+    User login(UserLoginRequest userLoginRequest);
 }
